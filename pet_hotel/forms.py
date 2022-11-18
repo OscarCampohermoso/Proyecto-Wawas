ac2f375f33ab7151121a58ffb99ab45e9173fb17
Fabi Martinez <104600297+FabiolaMartinez1@users.noreply.github.com>
2022-11-18 00:20:39 -0400

    v3 CRUD contactos

--- pet_hotel/forms.py
+++ pet_hotel/forms.py
@@ -3,7 +3,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.db import transaction
 from django.forms.utils import ValidationError
 
-from pet_hotel.models import User, Customer
+
+from pet_hotel.models import User, Customer, Contact
 
 
 class CustomerSignUpForm(UserCreationForm):
@@ -38,3 +39,26 @@ class AdminSignUpForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
+class ContactForm(forms.ModelForm):
+    '''name = forms.CharField(max_length=50, required=True)
+    email = forms.CharField(max_length=320, required=True)
+    phone = forms.CharField(max_length=999999999, required=True)
+    message = forms.CharField(max_length=600, required=True)
+'''
+    class Meta:
+        model = Contact
+        fields = '__all__'
+
+        labels = {
+        'name': 'Nombre',
+        'email': 'Correo Electrónico',
+        'phone': 'Número de telefono',
+        'message': 'Mensaje'
+        }
+        widgets = {
+            'name': forms.TextInput(attrs={'class':'form-control'}),
+            'email': forms.TextInput(attrs={'class':'form-control'}),
+            'phone': forms.TextInput(attrs={'class':'form-control'}),
+            'message': forms.Textarea(attrs={'class':'form-control'}),
+        }
