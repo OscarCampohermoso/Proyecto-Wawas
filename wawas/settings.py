7cf86820244966cdbafc2214b52af963db11ca81
Oscar Campohermoso <oscarcampohermoso8@gmail.com>
2022-12-08 00:17:05 -0400

    Configuración HOST

--- wawas/settings.py
+++ wawas/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-e9^^+_xgbz@1em7weowev=-z_-_5os7eqn6xs&6f8d-g)4r##n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['142.93.194.169']
 
 
 # Application definition
@@ -122,14 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = 'media/'
 
